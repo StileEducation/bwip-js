5076f6b6a7130cf70bd22bc997d289ac9b4beb1c
Charli Posner <charli.posner@stileeducation.com>
2025-08-21 12:19:56 +1000

    added eclevel and version type definitions to BwippOptions in bwip-js.d.ts

--- src/bwip-js.d.ts
+++ src/bwip-js.d.ts
@@ -65,6 +65,9 @@ declare namespace BwipJs {
         guardrightpos?: number | undefined;
         guardleftypos?: number | undefined;
         guardrightypos?: number | undefined;
+
+        eclevel?: 'L' | 'M' | 'Q' | 'H' | undefined;
+        version?: 'M1' | 'M2' | 'M3' | 'M4' | 'R7x43' | 'R7x59' | 'R7x77' | 'R7x99' | 'R7x139' | 'R9x43' | 'R9x59' | 'R9x77' | 'R9x99' | 'R9x139' | 'R11x27' | 'R11x43' | 'R11x59' | 'R11x77' | 'R11x99' | 'R11x139' | 'R13x27' | 'R13x43' | 'R13x59' | 'R13x77' | 'R13x99' | 'R13x139' | 'R15x43' | 'R15x59' | 'R15x77' | 'R15x99' | 'R15x139' | 'R17x43' | 'R17x59' | 'R17x77' | 'R17x99' | 'R17x139' | number | undefined;
     }
     export interface RenderOptions extends BwippOptions {
         bcid: string;
